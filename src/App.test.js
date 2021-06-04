9e4fb9b997adf53acea70b1f85c6b5d55ddd13fd
anznk <anzunakayama@gmail.com>
2021-06-04 11:48:50 -0700

    fixed conflict

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,16 @@
-<<<<<<< HEAD
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-=======
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+// =======
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
->>>>>>> origin/main
+
   expect(linkElement).toBeInTheDocument();
 });
